86fcfdac892bd013635212b8b917bf4024825084
Ali Rehan Haider <007rehan16@gmail.com>
2025-05-11 22:12:29 +0530

    Fix ESLint errors and ensure successful build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import { motion } from "framer-motion";
 import SocialIcons from "../components/SocialIcons";
 import Image from "next/image";
